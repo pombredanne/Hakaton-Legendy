2e36328b544e97eecff52384033d791ebf3ffc26
Bartosz Kosowski <genkaku@tower.multiverse>
2013-03-16 16:15:25 +0100

    Szkielet Generatora Zapytan #1230

---
+++ QueryGenerator/QueryGenerator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+""" Generuje zbiór zapytań na podstawie tekstu legendy. """
+
+def generate_queries(legend, corpus_dir):
+    """ Generuje zbiór list będących zapytaniami do wyszukiwarek. """
+    
+
+def find_rare_words(lemmatized_text):
+    """ Wyszukuje rzadkowystępujące (w j. polskim) słowa zawarte
+    w tekście (stringu) lemmatized_text. """
+    
+def find_keywords(rare_words, corpus_dir):
+    """ Wyszukuje słowa kluczowe zawarte w rare_keywords, takie, które
+    występują w tekstach (plikach) w katalogu corpus_dir. """
+    
