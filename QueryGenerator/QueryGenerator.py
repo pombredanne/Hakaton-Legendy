54701bd821d6177940d76fa02e1328c5fc916f86
Bartosz Kosowski <genkaku@tower.multiverse>
2013-03-18 19:57:04 +0100

    Generator korzysta ze słownika 'lemów'. Test lematyzacji.

--- QueryGenerator/QueryGenerator.py
+++ QueryGenerator/QueryGenerator.py
@@ -1,16 +1,67 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-""" Generuje zbiór zapytań na podstawie tekstu legendy. """
+import nltk
 
-def generate_queries(legend, corpus_dir):
-    """ Generuje zbiór list będących zapytaniami do wyszukiwarek. """
+class QueryGenerator:
+    """ Generuje zbiór zapytań na podstawie tekstu legendy. """
     
-
-def find_rare_words(lemmatized_text):
-    """ Wyszukuje rzadkowystępujące (w j. polskim) słowa zawarte
-    w tekście (stringu) lemmatized_text. """
+    def __init__(self, frequencies, lemmas):
+        """Konstruktor. 
+            frequencies to słownik {słowo : liczba} częstości występowania
+            słów w języku polskim. 
+            lemmas to słownik {słowo : słowo_w_postaci_podstawowej}, czyli
+            mapowanie ze słów do 'lemów'. """
+        self.frequencies = frequencies
+        self.lemmas = lemmas
+            
+            
+    def lemmatise(self, legend_toks_list):
+        """ Sprowadza tekst legendy do postaci zlematyzowanej. 
+            legend_toks_list to lista tokenów legendy. """
+        
+        lemmatized = []
+        
+        for token in legend_toks_list:
+            tok_low = token.lower()
+            if self.lemmas.has_key(tok_low):
+                lemmatized.append(self.lemmas[tok_low])
+            else:
+                lemmatized.append(tok_low)
+                
+        return lemmatized
+        
     
-def find_keywords(rare_words, corpus_dir):
-    """ Wyszukuje słowa kluczowe zawarte w rare_keywords, takie, które
-    występują w tekstach (plikach) w katalogu corpus_dir. """
+    def toks(selfs, legend):
+        """ Tokenizuje tekst legendy. """
+        # tokens = nltk.word_tokenize(legend)
+        pattern = r'''(?x)
+             ([A-Z]\.)+
+           | \w+    
+           | \w+(-\w+)*        
+           | \$?\d+(\.\d+)?%?  
+           | [][.,;"'?():-_`]'''
+        
+        p2 = r'(?x)\w+'
+        
+        p3 = r'\w+'
+        
+        return nltk.regexp_tokenize(legend, p3)
+        
+        #return nltk.word_tokenize(legend)
+    
+    def generate_queries(self, legend, corpus_dir):
+        """ Generuje zbiór list będących zapytaniami do wyszukiwarek. """
+        tokens = toks(self, legend)
+        
+        
+        
+    def find_rare_words(self, lemmatized_text):
+        """ Wyszukuje rzadkowystępujące (w j. polskim) słowa zawarte
+        w tekście (stringu) lemmatized_text. """
+        
+        
+    def find_keywords(self, rare_words, corpus_dir):
+        """ Wyszukuje słowa kluczowe zawarte w rare_keywords, takie, które
+        występują w tekstach (plikach) w katalogu corpus_dir. """
     
---
+++ QueryGenerator/QueryGeneratorTest.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+"""
+Testuje działanie generatora zapytań. 
+"""
+
+from QueryGenerator import QueryGenerator
+
+class Task206Test(unittest.TestCase):
+    """ Testy do generatora zapytań. """
+    
+    def testToks(self):
+        """ Sprawdza tokenizację. """
+        legenda = 'Moja babcia ma bardzo dużo lat. Hola! Powiedziała babcia mi.'
+       # legenda = legenda.decode('utf-8')
+        
+        tokens = ['Moja', 'babcia', 'ma', 'bardzo', 
+                    'dużo', 'lat', 'Hola', 
+                    'Powiedziała', 'babcia', 'mi' ]
+      #  for t in tokens:
+      #      t = t.decode('utf-8')
+        
+        frequencies = {
+            u'mieć': 0.012}
+        lemmas = {u'kosmici' : u'kosmita',
+                  u'mi' : u'mi'}
+        generator = QueryGenerator(frequencies, lemmas)
+        
+        gen_tokens = generator.toks(legenda)
+        
+        self.failUnlessEqual(tokens, gen_tokens)
+        
+    
+    def testLemmatization(self):
+        """ Sprawdza poprawność lematyzacji. """
+        
+        tokens = [u'Moja', u'babcia', u'ma', u'bardzo', 
+                    u'dużo', u'lat', u'Hola', 
+                    u'Powiedziała', u'babcia', u'mi' ]
+        
+        lemmatized_ref = [u'moje', u'babcia', u'mieć', u'bardzo',
+                          u'dużo', u'lat', u'hola', u'powiedzieć',
+                          u'babcia', u'ja']
+        
+        frequencies = {
+            u'mieć': 0.012}
+        lemmas = {u'kosmici' : u'kosmita',
+                  u'mi' : u'ja',
+                  u'moja' : u'moje',
+                  u'ma' : u'mieć',
+                  u'powiedziała' : u'powiedzieć'}
+        
+        generator = QueryGenerator(frequencies, lemmas)
+        
+        lemmatized_gen = generator.lemmatise(tokens)
+        
+        self.failUnlessEqual(lemmatized_ref, lemmatized_gen)
+        
+
+    def testRareWords(self):
+        """ Sprawdza poprawność znajdowania rzadkich słów. """
+        
+        lemmatized_text = u'babcia mieć kot śmierć w autostopowicz'
+        
+        frequencies = {
+            u'mieć': 0.012,
+            u'kot': 0.0003,
+            u'babcia': 0.006,
+            u'autostopowicz': 0.00005,
+            u'śmierć': 0.00002,
+            u'w' : 0.005
+            }
+        
+        lemmas = {u'kosmici' : u'kosmita',
+                  u'mi' : u'mi'}
+        
+        rares = {u'autostopowicz', u'śmierć'}
+        
+        generator = QueryGenerator(frequencies, lemmas)
+        
+        rares_gen = generator.find_rare_words(lemmatized_text)
+        
+        self.failUnlessEqual(rares, rares_gen)
+
+
+if __name__ == '__main__':
+    unittest.main()
