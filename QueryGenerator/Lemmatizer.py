415fd3e62b6223ce66f4dee26b8469252148d48d
Krzysztof Wisznarewski <kw40102@st.amu.edu.pl>
2013-03-27 21:22:27 +0100

    usprawiony slownik + dzialajacy poprawnie lematyzator

--- QueryGenerator/Lemmatizer.py
+++ QueryGenerator/Lemmatizer.py
@@ -3,41 +3,38 @@
 
 import codecs
 import os
-import re
 
 
 class Lemmatizer:
+    file_name = os.path.abspath('dictionaries/slownik_lemm.txt')
     """Konstruktor"""
     def __init__(self):
-        file_name = os.path.abspath('dictionaries/slownik_lemm.txt')
-        self.searchfile = codecs.open(file_name, 'r', 'utf-8')
+        
         self.lems = dict()
 
 
     """Znajduje forme podstawową argumentu słowo"""
     def znajdz(self, slowo):
+        self.searchfile = codecs.open(self.file_name, 'r', 'utf-8')
         for line in self.searchfile:
-            if self.findWholeWord(slowo)(line):
+            if slowo in line:
                 words = line.split()
                 tempik = words[0]
                 podst = tempik.replace(",", "")
                 self.lems[slowo] = podst
                 break
+        self.searchfile.close()
 
 
     def get_lems(self):
         return self.lems
 
-    def findWholeWord(self, w):
-        return re.compile(r'\b({0})\b'.format(w)).search
-
-'''
 lemmaitzed = []
 
 lemik = Lemmatizer()
-lemik.znajdz('ma')
-lemik.znajdz("babijami")
-print lemik.get_lems()
-print u'ad\u017cma\u0144ski'.encode('utf-8')
+lemik.znajdz(" babijami, ")
+lemik.znajdz(" ma, ")
 
-'''
+
+print lemik.get_lems()
+print u'mie\u0107'.encode('utf-8')
